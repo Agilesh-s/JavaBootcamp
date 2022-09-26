59a68fa72207f0c9b81fc5b107c879929e1d5794
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-09-26 15:19:35 +0530

    menudrivencommit

---
+++ src/com/nest/menuDriven.java
@@ -0,0 +1,40 @@
+package com.nest;
+
+import java.util.Scanner;
+
+public class menuDriven {
+    public static void main(String[] args) {
+        System.out.println("Menu Driven");
+        int choice;
+
+        while (true){
+            System.out.println("Select an Option");
+            System.out.println("1. Add Students");
+            System.out.println("2. Search Students");
+            System.out.println("3. Delete Students");
+            System.out.println("4. View all Students");
+            System.out.println("5. Exit");
+
+            Scanner input = new Scanner(System.in);
+            choice = input.nextInt();
+
+            switch (choice){
+                case 1 :
+                    System.out.println("Add Student Selected");
+                    break;
+                case 2:
+                    System.out.println("Search Student Selected");
+                    break;
+                case 3:
+                    System.out.println("Delete Student");
+                    break;
+                case 4:
+                    System.out.println("View All The Student");
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        }
+
+    }
+}
